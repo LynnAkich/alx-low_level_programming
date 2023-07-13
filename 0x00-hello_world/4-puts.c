ef470c80ae97cde46ceed72190e4868f96554d5f
LynnAkich <lynnakich@gmail.com>
2023-07-13 15:11:46 -0700

    C program with output message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzlet");
+        puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
