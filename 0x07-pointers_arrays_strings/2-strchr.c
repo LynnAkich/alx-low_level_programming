694b0fa818a824197cc21359f9e37a4a9e6ea007
LynnAkich <lynnakich@gmail.com>
2023-07-31 09:41:13 -0700

    Locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string
+ * @s: input value
+ * @c: input value
+ * Return: 0
+ */
+char *_strchr(char *s, char c)
+{
+	int a = 0;
+
+	for (; s[a] >= '\0'; a++)
+	{
+		if (s[a] == c)
+			return (&s[a]);
+	}
+	return (0);
+}
